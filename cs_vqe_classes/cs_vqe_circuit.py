866261f0c4d40c4f9669bbf9317c327e0a412500
Tim <timothy.weaving.20@ucl.ac.uk>
2021-07-25 22:27:05 +0100

    New class for constructing cs_vqe circuits and plotting tools for analysis

---
+++ cs_vqe_classes/cs_vqe_circuit.py
@@ -0,0 +1,182 @@
+from cs_vqe_classes.cs_vqe import cs_vqe
+from cs_vqe_classes.eigenstate import eigenstate
+import utils.bit_tools as bit
+import utils.cs_vqe_tools as c_tools
+import utils.circuit_tools as circ
+import utils.qonversion_tools as qonvert
+from copy import deepcopy
+
+from qiskit.circuit.parameter import Parameter
+from qiskit.circuit.quantumcircuit import QuantumCircuit
+from qiskit.utils import QuantumInstance
+from qiskit.aqua.components.optimizers import SLSQP, COBYLA
+from qiskit.algorithms import VQE
+from qiskit import Aer
+from openfermion.linalg import get_sparse_operator, get_ground_state
+
+
+class cs_vqe_circuit():
+    """
+    """
+    # class variables for storing VQE results
+    counts = []
+    values = []
+    
+    def __init__(self, hamiltonian, terms_noncon, num_qubits, order, rot=True):
+        self.num_qubits = num_qubits
+        self.order = deepcopy(order)
+        self.rot = rot
+        # epistricted model and reduced Hamiltonian
+        cs = cs_vqe(hamiltonian, terms_noncon, num_qubits)
+        generators = cs.generators(rot=True)
+        # defined here so only executed once instead of at each call
+        self.ham_reduced = cs.reduced_hamiltonian(order)
+        self.gs_noncon_energy = cs.gs_noncon_energy()
+        self.true_gs = cs.true_gs()[0]
+        self.G = generators[0]
+        self.A = generators[1]
+        self.rotations = cs.rotations()
+        self.init_state = cs.init_state(rot=rot)
+        self.fixed_qubit = list(self.A.keys())[0].find('X')
+        # +1-eigenstate parameters
+        eig = eigenstate(self.A, bit.bin_to_int(self.init_state), num_qubits)
+        self.index_paulis = eig.P_index(q_pos=True)
+        self.t = eig.t_val()
+
+
+    def sim_qubits(self, num_sim_q, complement=False):
+        """
+        """
+        q_pos_ord = [self.num_qubits-1-q for q in self.order]
+        if complement:
+            sim_indices = self.order[num_sim_q:]
+            sim_qubits = q_pos_ord[num_sim_q:]
+        else:
+            sim_indices = self.order[0:num_sim_q]
+            sim_qubits = q_pos_ord[0:num_sim_q]
+        # if ordering left to right
+        if sim_indices != []:
+            sim_qubits, sim_indices = zip(*sorted(list(zip(sim_qubits, sim_indices)), key=lambda x:x[1]))
+
+        return sim_qubits, sim_indices
+
+
+    def index_paulis_reduced(self, num_sim_q):
+        """
+        """
+        P_indices = [q for q in self.index_paulis['Z1'] if q in self.sim_qubits(num_sim_q)[0]]
+        return P_indices 
+
+
+    def lost_parity(self, num_sim_q):
+        """
+        """
+        lost_parity = 0
+        for i in self.sim_qubits(num_sim_q, complement=True)[1]:
+            lost_parity += int(self.init_state[i])
+
+        return lost_parity%2
+
+    
+    def reduce_anz_terms(self, anz_terms, num_sim_q):
+        """
+        """
+        if self.rot:
+            if type(anz_terms)!=dict:
+                anz_terms = {t:1 for t in anz_terms}
+            anz_terms = c_tools.rotate_operator(self.rotations, anz_terms)
+        
+        anz_terms_reduced = []
+        for p in anz_terms.keys():
+            blank_op = ['I' for i in range(num_sim_q)]
+            for i, sim_i in enumerate(self.sim_qubits(num_sim_q)[1]):
+                if sim_i != self.fixed_qubit:
+                    blank_op[i] = p[sim_i]
+            if set(blank_op) != {'I'}:
+                anz_terms_reduced.append('I'+''.join(blank_op))
+
+        return anz_terms_reduced
+
+    
+    def build_circuit(self, anz_terms, num_sim_q, trot_order=2):
+        """
+        """
+        anz_terms = self.reduce_anz_terms(anz_terms, num_sim_q)
+        sim_qubits = self.sim_qubits(num_sim_q)[0]
+        index_paulis_reduced = self.index_paulis_reduced(num_sim_q)
+        lost_parity = self.lost_parity(num_sim_q)
+
+        qc = QuantumCircuit(num_sim_q+1)
+
+        # only applies for HeH+, needs to be generalised for init_state
+        try:
+            q4_pos = num_sim_q - 1 - (sim_qubits).index(4)
+            qc.x(q4_pos)
+        except:
+            print('q4 not simulated')
+
+        if set(anz_terms) == {'II'} or anz_terms == []:
+            # because VQE requires at least one parameter...
+            qc.rz(Parameter('a'), 0)
+        else:
+            qc = circ.circ_from_paulis(paulis=set(anz_terms), circ=qc, trot_order=trot_order, dup_param=True)
+        
+        if lost_parity:
+            qc.x(num_sim_q)
+            
+        cascade_bits = []
+        for i in index_paulis_reduced:
+            cascade_bits.append(num_sim_q - 1 - sim_qubits.index(i))
+
+        #store parity in ancilla qubit (8) via CNOT cascade
+        qc = circ.cascade(cascade_bits+[num_sim_q], circ=qc)
+
+        qc.x(0)
+        #move into +1-eigenstate
+        qc.cry(-2*self.t, num_sim_q, 0)
+        qc.x(num_sim_q)
+        qc.cry(2*self.t, num_sim_q, 0)
+        qc.x(num_sim_q)
+
+        #reverse CNOT cascade
+        qc = circ.cascade(cascade_bits+[num_sim_q], circ=qc, reverse=True)
+
+        if lost_parity:
+            qc.x(num_sim_q)
+
+        return qc
+
+
+    def store_intermediate_result(self, eval_count, parameters, mean, std):
+        """
+        """
+        self.counts.append(eval_count)
+        self.values.append(mean)
+
+
+    def CS_VQE(self, anz_terms, num_sim_q, optimizer=SLSQP(maxiter=1000)):
+        """
+        """
+        self.counts.clear()
+        self.values.clear()
+
+        qi = QuantumInstance(Aer.get_backend('statevector_simulator'))
+        qc = self.build_circuit(anz_terms, num_sim_q)
+        vqe = VQE(qc, optimizer=optimizer, callback=self.store_intermediate_result, quantum_instance=qi)
+        
+        h_add_anc={}
+        
+        for p in (self.ham_reduced[num_sim_q-1]).keys():
+            p_add_q = 'I' + p
+            h_add_anc[p_add_q] = self.ham_reduced[num_sim_q-1][p]
+
+        vqe_input_ham = qonvert.dict_to_WeightedPauliOperator(h_add_anc)
+        ham_red_q = qonvert.dict_to_QubitOperator(h_add_anc)
+        gs_red = get_ground_state(get_sparse_operator(ham_red_q, num_sim_q+1).toarray())
+        target_energy = gs_red[0]
+
+        vqe_run = vqe.compute_minimum_eigenvalue(operator=vqe_input_ham)
+
+        return vqe_run.optimal_value, target_energy, self.counts, self.values
+
+        
---
+++ utils/plotting_tools.py
@@ -0,0 +1,96 @@
+import math
+import itertools
+import matplotlib.pyplot as plt
+import cs_vqe_classes.cs_vqe_circuit as cs_circ
+from statistics import median
+
+
+def factor_int(n):
+    val = math.ceil(math.sqrt(n))
+    val2 = int(n/val)
+    while val2 * val != float(n):
+        val -= 1
+        val2 = int(n/val)
+    # order the factors
+    if val > val2:
+        val, val2 = val2, val
+
+    return val, val2
+
+
+def plot_cs_vqe_convergence(hamiltonian, terms_noncon, anz_terms, num_qubits, order, max_sim_q):
+    rows, cols = factor_int(max_sim_q)
+    fig, axs = plt.subplots(nrows = rows, ncols = cols, figsize = (6*cols,6*rows))
+    if rows == 1:
+        grid_pos = range(max_sim_q)
+    else:
+        grid_pos = list(itertools.product(range(rows), range(cols)))
+
+    circs = cs_circ.cs_vqe_circuit(hamiltonian, terms_noncon, num_qubits, order)
+    gs_noncon_energy = circs.gs_noncon_energy
+    true_gs = circs.true_gs
+
+    for index, grid in enumerate(grid_pos):
+        num_sim_q = index+1
+        vqe_result, target_energy, X, Y = circs.CS_VQE(anz_terms, num_sim_q)
+
+        # plot results in corresponding subfigure
+        l1 = axs[grid].plot(X, Y, color='black', zorder=2)
+        l2 = axs[grid].hlines(gs_noncon_energy, X[0], X[-1], color='r', zorder=0)
+        l3 = axs[grid].hlines(true_gs, X[0], X[-1], color='g', zorder=1)
+        # creating legend labels for target and convergence value
+        l4 = axs[grid].plot([1], [0], color='purple', ls='--', zorder=3)
+        l5 = axs[grid].plot([1], [0], color='b', ls='--', zorder=4)
+
+        
+        axs[grid].set_xticks(X)
+        if index == num_qubits:
+            axs[grid].set_title("Full VQE")
+        else:  
+            axs[grid].set_title("%i qubits + ancilla" % num_sim_q)
+        axs[grid].set_xticklabels([])
+        
+        # plotting zoomed portion of graph to observe convergence
+        X_zoom = []
+        Y_zoom = []
+        for index, t in enumerate(Y):
+            if t-0.01 < gs_noncon_energy:
+                X_zoom.append(X[index])
+                Y_zoom.append(t)
+
+        # location for the zoomed portion
+        ax_box = axs[grid].get_position()
+        ax_origin = (ax_box.get_points())[1]
+        sf_size = 0.1
+        sub_axes = plt.axes([ax_origin[0]-sf_size*1.1, ax_origin[1]-sf_size*1.4, sf_size*1.1, sf_size*1.4])
+
+        # plot the zoomed portion
+        sub_axes.set_ylim((true_gs-0.01, gs_noncon_energy+0.01))
+        sub_axes.plot(X_zoom, Y_zoom, color='black')
+        # lines fixed at noncontextual ground energy and true ground energy
+        sub_axes.hlines(gs_noncon_energy, X_zoom[0], X_zoom[-1], color='r')
+        sub_axes.hlines(true_gs, X_zoom[0], X_zoom[-1], color='g')
+        # plotting the target value given the number of qubits simulated
+        sub_axes.hlines(target_energy, X_zoom[0], X_zoom[-1], color='purple', ls='--')
+        sub_axes.text(x=median(X_zoom), y=target_energy-0.004, s= 'target = '+str(round(target_energy, 4)), size='small')
+        # plotting the convergence value
+        sub_axes.hlines(vqe_result, X_zoom[0], X_zoom[-1], color='b', ls='--')
+        sub_axes.text(x=X_zoom[0], y=Y[-1]-0.004, s= 'min = '+str(round(Y[-1], 4)), size='small')
+        
+        if rows != 1:
+            if grid[0] == 1:
+                axs[grid].set_xlabel('Optimisation count',fontsize=16)
+            if grid[1] == 0:
+                axs[grid].set_ylabel('Energy (Ha)',fontsize=18)
+        else:
+            axs[grid].set_xlabel('Optimisation count',fontsize=16)
+            if grid == 0:
+                axs[grid].set_ylabel('Energy (Ha)',fontsize=18)
+    
+    fig.legend([l1,l2,l3,l4,l5],
+            labels=['CS-VQE optimisation','Target Value','Convergence Value','Noncontextual ground state energy','True ground state energy'],
+            loc="lower center",   # Position of legend
+            borderaxespad=0.1,    # Small spacing around legend box
+            ncol=5)
+
+    return fig
