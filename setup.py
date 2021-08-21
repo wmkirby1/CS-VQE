04f4ad4f6d2c9f84a4e8ee37494f595524f68d6b
TimWeaving <72549389+TimWeaving@users.noreply.github.com>
2021-08-21 18:25:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,8 +16,7 @@ setuptools.setup(
         include=["utils", "cs_vqe_classes", "steps"]),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
+        "Operating System :: OS Independent"],
     setup_requires=["setuptools_scm~=6.0"],
     install_requires = ["openfermionpyscf @ git+ssh://git@github.com/quantumlib/OpenFermion-PySCF.git"]
 )
