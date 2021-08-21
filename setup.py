8b4ddfc74ddb342887f91632bfcee840956d6a33
TimWeaving <72549389+TimWeaving@users.noreply.github.com>
2021-08-21 18:48:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/TimWeaving/CS-VQE",
     packages=setuptools.find_namespace_packages(
-        include=["utils", "cs_vqe_classes", "steps"]),
+        include=["utils", "cs_vqe_classes", "steps", "openfermionpyscf"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent"],
     setup_requires=["setuptools_scm~=6.0"],
-    dependency_links=['https://github.com/quantumlib/OpenFermion-PySCF/tarball/master#egg=openfermionpyscf-1.0']
+    dependency_links=['https://github.com/quantumlib/OpenFermion-PySCF/tarball/master#egg=OpenFermion-PySCF-1.0']
 )
