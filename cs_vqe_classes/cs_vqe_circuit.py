922a65087a86c46ec0a9b6d0f8e279733ea12320
Tim <timothy.weaving.20@ucl.ac.uk>
2021-08-23 03:36:25 +0100

    Resolving optimiser conflict for push

--- cs_vqe_classes/cs_vqe_circuit.py
+++ cs_vqe_classes/cs_vqe_circuit.py
@@ -472,7 +472,7 @@ class cs_vqe_circuit():
         self.errors.append(std)
 
 
-    def CS_VQE(self, anz_terms=None, num_sim_q=None, ham=None, optimizer=POWELL(maxiter=10000), check_A=False, noise=False):
+    def CS_VQE(self, anz_terms=None, num_sim_q=None, ham=None, optimizer=IMFIL(maxiter=10000), check_A=False, noise=False):
         """
         """
         self.counts.clear()
@@ -578,7 +578,7 @@ class cs_vqe_circuit():
                 'errors':errors}
 
 
-    def run_cs_vqe(self, anz_terms=None, max_sim_q=None, min_sim_q=0, optimizer=POWELL(maxiter=10000), iters=1, check_A=False, noise=False):
+    def run_cs_vqe(self, anz_terms=None, max_sim_q=None, min_sim_q=0, optimizer=IMFIL(maxiter=10000), iters=1, check_A=False, noise=False):
         """
         """
         if max_sim_q is None:
