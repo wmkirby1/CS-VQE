3b5fb2084a4f39020695e37c09a76c95a61fb325
Tim <timothy.weaving.20@ucl.ac.uk>
2021-09-28 22:12:18 +0100

    test json

--- utils/orquestra/ansatz_circuit.py
+++ utils/orquestra/ansatz_circuit.py
@@ -1,9 +1,15 @@
+import json
 import cs_vqe_classes.cs_vqe_circuit as cs_circ
 from qusetta import Qiskit
 from zquantum.core.circuits import (save_circuit,import_from_cirq)
 
 def ansatz_circuit(ham, terms_noncon, anz_op, num_qubits, num_sim_q):
+    print('-------------------------checkpoint--------------------------')
     print(terms_noncon, type(terms_noncon))
+    try:
+        print(json.load(terms_noncon))
+    except:
+        print("Could not parse json")
     print('-------------------------checkpoint--------------------------')
     mol_circ = cs_circ.cs_vqe_circuit(hamiltonian=ham,
                                     terms_noncon=terms_noncon,
