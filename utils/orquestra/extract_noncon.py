952c3cd5b3b524bd20e7bbdb4c2a89b60b039f0f
Tim <timothy.weaving.20@ucl.ac.uk>
2021-09-28 23:56:38 +0100

    change to save_list

--- utils/orquestra/extract_noncon.py
+++ utils/orquestra/extract_noncon.py
@@ -1,5 +1,25 @@
 import utils.cs_vqe_tools as cs_tools
-from zquantum.core.utils import save_list
+import json
+from typing import List, Dict, Any
+from zquantum.core.typing import AnyPath
+
+SCHEMA_VERSION = "zapata-v1"
+
+
+def save_list(array: List, filename: AnyPath, artifact_name: str = ""):
+    """Save expectation values to a file.
+    Args:
+        array (list): the list to be saved
+        file (str or file-like object): the name of the file, or a file-like object
+        artifact_name (str): optional argument to specify the schema name
+    """
+    dictionary: Dict[str, Any] = {}
+    dictionary["schema"] = SCHEMA_VERSION + "-" + artifact_name + "-list"
+    dictionary["list"] = array
+
+    with open(filename, "w") as f:
+        f.write(json.dumps(dictionary, indent=2))
+
 
 def extract_noncon(ham, search_time=10, criterion='weight'):
     terms_noncon = cs_tools.greedy_dfs(ham, search_time, criterion)[-1]
