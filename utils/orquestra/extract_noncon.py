56fdf97390668089b0a637ce51313c5f30109213
Tim <timothy.weaving.20@ucl.ac.uk>
2021-09-28 23:30:59 +0100

    Different data model in workflow

--- utils/orquestra/extract_noncon.py
+++ utils/orquestra/extract_noncon.py
@@ -3,6 +3,6 @@ from utils.json_tools import *
 
 def extract_noncon(ham, search_time=10, criterion='weight'):
     terms_noncon = cs_tools.greedy_dfs(ham, search_time, criterion)[-1]
-    data_dict = {}
-    data_dict['noncon'] = terms_noncon
-    save_json(data_dict, 'noncon.json')
+    #data_dict = {}
+    #data_dict['noncon'] = terms_noncon
+    #save_json(data_dict, 'noncon.json')
