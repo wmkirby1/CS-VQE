d77180fcc1248a35248b0bdd808d3da3c31006c1
Tim <timothy.weaving.20@ucl.ac.uk>
2021-09-28 17:04:07 +0100

    qiskit integration functionality for orquestra

--- utils/orquestra/ansatz_circuit.py
+++ utils/orquestra/ansatz_circuit.py
@@ -1,4 +1,12 @@
 import cs_vqe_classes.cs_vqe_circuit as cs_circ
+from zquantum.core.circuits import (
+    Circuit,
+    load_circuit,
+    load_circuitset,
+    save_circuit,
+    save_circuitset,
+)
+from zquantum.core.circuits.conversions.qiskit_conversions import import_from_qiskit 
 
 def ansatz_circuit(ham, terms_noncon, anz_op, num_qubits, num_sim_q):
     
@@ -7,5 +15,7 @@ def ansatz_circuit(ham, terms_noncon, anz_op, num_qubits, num_sim_q):
                                     num_qubits=num_qubits)
 
     anz_circ = mol_circ.build_circuit(anz_op, num_sim_q)
+    anz_circ_zquantum = import_from_qiskit(anz_circ) 
+    save_circuit(anz_circ_zquantum, "circuit.json")
 
-    return anz_circ
+    return anz_circ_zquantum
