e7fca91c891a02277bebb0eb9c8f2425a707c97c
Tim <timothy.weaving.20@ucl.ac.uk>
2021-09-28 23:44:52 +0100

    Import .utils

--- utils/orquestra/extract_noncon.py
+++ utils/orquestra/extract_noncon.py
@@ -1,5 +1,5 @@
 import utils.cs_vqe_tools as cs_tools
-from zquantum.core import save_list
+from zquantum.core.utils import save_list
 
 def extract_noncon(ham, search_time=10, criterion='weight'):
     terms_noncon = cs_tools.greedy_dfs(ham, search_time, criterion)[-1]
