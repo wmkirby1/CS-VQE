47aba3aba5099306e8c76d5a2c1023d8d4fae974
Tim <timothy.weaving.20@ucl.ac.uk>
2021-09-29 15:30:18 +0100

    setup.py versions

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/TimWeaving/CS-VQE",
     packages=setuptools.find_namespace_packages(
-        include=["utils", "cs_vqe_classes", "hamiltonians", "steps", "openfermionpyscf", "openfermion", "cirq", "scikit-quant", "qusetta"]),
+        include=["utils", "cs_vqe_classes", "hamiltonians", "steps", "openfermionpyscf", "openfermion", "cirq", "scikit-quant"]),
     package_data={'utils': ['*.json']},
     include_package_data=True,
     classifiers=[
@@ -23,6 +23,7 @@ setuptools.setup(
     install_requires=['openfermionpyscf @ https://github.com/quantumlib/OpenFermion-PySCF/tarball/master#egg=openfermionpyscf-1.0',
                       'openfermion @ https://github.com/quantumlib/OpenFermion/tarball/master#egg=openfermion-1.0.1',
                       'cirq @ https://github.com/quantumlib/Cirq/tarball/master#egg=cirq-0.10.0',
-                      'scikit-quant @ https://github.com/scikit-quant/scikit-quant/tarball/master#egg=scikit-quant-0.7.0',
-                      'qusetta @ https://github.com/qcware/qusetta/tarball/master#egg=qusetta-0.0.0']
+                      'scikit-quant @ https://github.com/scikit-quant/scikit-quant/tarball/master#egg=scikit-quant-0.7.0']
 )
+
+print(openfermion.__version__)
