2dfa8176638a76fdbe5ff448a76ece88703c3191
Tim <timothy.weaving.20@ucl.ac.uk>
2021-09-29 14:51:26 +0100

    Specify older versions of cirq and openfermion

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ setuptools.setup(
         "Operating System :: OS Independent"],
     setup_requires=["setuptools_scm~=6.0"],
     install_requires=['openfermionpyscf @ https://github.com/quantumlib/OpenFermion-PySCF/tarball/master#egg=openfermionpyscf-1.0',
-                      'openfermion @ https://github.com/quantumlib/OpenFermion/tarball/master#egg=openfermion-1.1.0',
-                      'cirq @ https://github.com/quantumlib/Cirq/tarball/master#egg=cirq-0.11.0',
+                      'openfermion @ https://github.com/quantumlib/OpenFermion/tarball/master#egg=openfermion-1.0.1',
+                      'cirq @ https://github.com/quantumlib/Cirq/tarball/master#egg=cirq-0.10.0',
                       'scikit-quant @ https://github.com/scikit-quant/scikit-quant/tarball/master#egg=scikit-quant-0.7.0',
                       'qusetta @ https://github.com/qcware/qusetta/tarball/master#egg=qusetta-0.0.0']
 )
