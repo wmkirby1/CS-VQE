0a10f56d4cb845dd4ecae4a5779db10c20dc286d
Tim <timothy.weaving.20@ucl.ac.uk>
2021-09-29 11:24:48 +0100

    qusseta cirq not working

--- utils/orquestra/ansatz_circuit.py
+++ utils/orquestra/ansatz_circuit.py
@@ -1,6 +1,6 @@
 import json
 import cs_vqe_classes.cs_vqe_circuit as cs_circ
-from qusetta import Qiskit
+#from qusetta import Qiskit
 from zquantum.core.circuits import (save_circuit,import_from_cirq)
 
 def ansatz_circuit(ham, terms_noncon, anz_op, num_qubits, num_sim_q):
@@ -16,6 +16,7 @@ def ansatz_circuit(ham, terms_noncon, anz_op, num_qubits, num_sim_q):
                                     num_qubits=num_qubits)
 
     anz_circ = mol_circ.build_circuit(anz_op, num_sim_q)
-    anz_circ_cirq = Qiskit.to_cirq(anz_circ)
-    anz_circ_zquantum = import_from_cirq(anz_circ_cirq) 
-    save_circuit(anz_circ_zquantum, "circuit.json")
+    print(anz_circ.draw())
+    #anz_circ_cirq = Qiskit.to_cirq(anz_circ)
+    #anz_circ_zquantum = import_from_cirq(anz_circ_cirq) 
+    #save_circuit(anz_circ_zquantum, "circuit.json")
