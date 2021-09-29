3074f413754ab2cffe67145121e390de4138e5a8
Tim <timothy.weaving.20@ucl.ac.uk>
2021-09-29 16:43:45 +0100

    openfermion version

--- setup.py
+++ setup.py
@@ -13,12 +13,13 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/TimWeaving/CS-VQE",
     packages=setuptools.find_namespace_packages(
-        include=["utils", "cs_vqe_classes", "hamiltonians", "steps", "scikit-quant"]),
+        include=["utils", "cs_vqe_classes", "hamiltonians", "steps", "scikit-quant", "openfermion"]),
     package_data={'utils': ['*.json']},
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent"],
     setup_requires=["setuptools_scm~=6.0"],
-    install_requires=['scikit-quant @ https://github.com/scikit-quant/scikit-quant/tarball/master#egg=scikit-quant-0.7.0']
+    install_requires=['scikit-quant @ https://github.com/scikit-quant/scikit-quant/tarball/master#egg=scikit-quant-0.7.0',
+                    'openfermion==1.0.1']
 )
